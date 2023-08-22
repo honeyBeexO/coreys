274401ca7956901db31c8b3313f4dc715c124331
nouhben <nouhben@MacBook-Pro.local>
2023-08-22 17:01:11 +0100

    adding routes

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#we specify the name because sometimes we want to do a reverse lookup on that route
+#using the specific name makes it easier because we could be having multiple routes with the same
+#name for example: Blog-> Home view, Store-> Home view. if we name them both home it wouldn't work 
+urls = [
+    path('',views.home, name='blog_home'),
+]
