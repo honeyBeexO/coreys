7ab995cb605bdc9e914095ae965c0666eb1396d0
nouhben <nouhben@MacBook-Pro.local>
2023-08-24 15:19:55 +0100

    templates & context dictionnnary

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'title':'ChatGPT',
+        'content':'skhskjdhsjkhdhdskhdkjbdsdkjshd',
+        'author':'Corey'
+    },
+    {
+        'title':'ChatGPT+',
+        'content':'skhskjdhsjkhdhdskhdkjbdsdkjshd',
+        'author':'Amin'
+    },
+    {
+        'title':'AI revolution',
+        'content':'skhskjdhsjkhdhdskhdkjbdsdkjshd',
+        'author':'Fouzia'
+    }
+]
 def home(request):
     #return HttpResponse('<h1>Blog Home Page View</h1>', data=[title])
     
-    return render(request, 'blog/home.html',context={'title':'Home'})
+    return render(request, 'blog/home.html',context={'title':'Home','posts':posts})
 
 def about(request):
     #return HttpResponse('<h1>Blog About Page Function View</h1>')
